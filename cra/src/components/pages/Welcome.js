95c0ce65587892d8c25332e78f014a4922c2b913
Ethan Storm <stormethan10@gmail.com>
2023-03-09 01:51:22 -0800

    Update Welcome.js

--- cra/src/components/pages/Welcome.js
+++ cra/src/components/pages/Welcome.js
@@ -19,7 +19,7 @@ class Welcome extends React.Component {
 				{!this.props.auth0.isAuthenticated ? (
 					<LoginButton />
 				) : (
-					<Button href='/game'>To the dungeon!</Button>
+					<button href='/game'>To the dungeon!</button>
 				)}
 			</Container>
 		)

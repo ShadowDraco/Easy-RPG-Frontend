1398afb77178640214f21de61191a0c40135a6d5
Ethan Storm <stormethan10@gmail.com>
2023-03-09 01:48:45 -0800

    Update Welcome.js

--- cra/src/components/pages/Welcome.js
+++ cra/src/components/pages/Welcome.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container'
 import LoginButton from '../auth0/LoginButton'
 
 import { withAuth0 } from '@auth0/auth0-react'
+import Button from 'react-bootstrap/Button'
 
 class Welcome extends React.Component {
 	constructor(props) {
@@ -15,7 +16,11 @@ class Welcome extends React.Component {
 		return (
 			<Container className='my-3'>
 				<h1>Welcome</h1>
-				{!this.props.auth0.isAuthenticated && <LoginButton />}
+				{!this.props.auth0.isAuthenticated ? (
+					<LoginButton />
+				) : (
+					<Button href='/game'>To the dungeon!</Button>
+				)}
 			</Container>
 		)
 	}

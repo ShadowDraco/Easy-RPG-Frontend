3a6b7e8893e87a26379eebb8060f951d590324a5
Ethan Storm <stormethan10@gmail.com>
2023-03-08 20:34:02 -0800

    Update index.js

--- cra/src/index.js
+++ cra/src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 				domain='dev-iaagx3vzh22w84ou.us.auth0.com'
 				clientId='Ad59Ar27j64zPw2jD6FMPLzlWw2z7bFR'
 				authorizationParams={{
-					redirect_uri: `${window.location.origin}/game`,
+					redirect_uri: `https://easy-rpg.netlify.app/game`,
 					//audience: `${import.meta.env.SERVER_URL}`,
 					//scope: 'read:current_user update:current_user_metadata',
 				}}
